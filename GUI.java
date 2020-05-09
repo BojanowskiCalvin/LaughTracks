a8569baff682aa8e517e74b4c5879c1741e84924
bojanowskicalvin <bojanowskicalvin@tutanota.com>
2020-05-09 16:08:04 -0400

    Added functioning laugh button

--- GUI.java
+++ GUI.java
@@ -1,33 +1,54 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+public class GUI {
 
-public class GUI{
-
-    public GUI(){
+    public GUI() {
         JFrame frame = new JFrame();
-        frame.setSize(350,200);
+        frame.setSize(350, 200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
 
         JPanel panel = new JPanel();
         placeComponents(panel);
         frame.add(panel);
-      
+
     }
-    private static void placeComponents(JPanel panel){
+
+    private static void placeComponents(JPanel panel) {
 
         panel.setLayout(null);
         JLabel label = new JLabel("Laugh Tracks");
 
-        label.setBounds(125,20,100,25);
+        label.setBounds(125, 20, 100, 25);
         panel.add(label);
 
         JButton laughbutton = new JButton("laugh");
-        laughbutton.setBounds(125,80,100,25);
+        laughbutton.setBounds(125, 80, 100, 25);
+        laughbutton.addActionListener((ActionListener) new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                try {
+                    Laugh clicked_laugh = new Laugh();
+                    clicked_laugh.PlayLaugh();
+                } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+
+        });
         panel.add(laughbutton);
+
     }
     public static void main(String[] args){
         new GUI();
---
+++ Laugh.java
@@ -0,0 +1,74 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+public class Laugh {
+    String laugh_path="";
+    static Boolean no_laughs;
+    Clip clip;
+
+    public Laugh() 
+        throws UnsupportedAudioFileException,IOException,
+        LineUnavailableException{
+        no_laughs = true;
+        laugh_path = RandomLaugh();
+        AudioInputStream audioInputStream = 
+        AudioSystem.getAudioInputStream(new File("resources/laughs/"+laugh_path));
+        
+        clip = AudioSystem.getClip();
+        clip.open(audioInputStream);
+    }
+
+
+
+
+    private static String RandomLaugh() {
+        try {
+            no_laughs = true;
+            String[] pathnames;
+
+            File laugh_directory = new File("resources/laughs");
+
+            pathnames = laugh_directory.list();
+
+            if (pathnames.length > 0) {
+                int rnd = new Random().nextInt(pathnames.length);
+                no_laughs = false;
+            return pathnames[rnd];
+        }
+        else{
+            return "No Laughs :(";
+        }
+    }
+    catch(Exception e){
+        return "No Laughs :(";
+    }
+        
+    }
+    public String getLaughPath(){
+        try{
+        return laugh_path;
+        }
+        catch (Exception e){
+            return "No Laughs :(";
+        }
+    }
+    public Boolean checkLaughs(){
+        if (no_laughs == true){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+    public void PlayLaugh(){
+        clip.start();
+
+    }
+}
