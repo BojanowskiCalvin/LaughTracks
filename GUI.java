b9e5a2fdf1555268e5323a2c12c14ddf4e439251
bojanowskicalvin <bojanowskicalvin@tutanota.com>
2020-05-09 15:11:06 -0400

    Basic UI

--- GUI.java
+++ GUI.java
@@ -1,16 +1,33 @@
-import javax.swing.border.*;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+
 
 public class GUI{
 
     public GUI(){
         JFrame frame = new JFrame();
-        frame.setSize(100,100);
+        frame.setSize(350,200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+
         JPanel panel = new JPanel();
+        placeComponents(panel);
+        frame.add(panel);
       
+    }
+    private static void placeComponents(JPanel panel){
+
+        panel.setLayout(null);
+        JLabel label = new JLabel("Laugh Tracks");
+
+        label.setBounds(125,20,100,25);
+        panel.add(label);
+
+        JButton laughbutton = new JButton("laugh");
+        laughbutton.setBounds(125,80,100,25);
+        panel.add(laughbutton);
     }
     public static void main(String[] args){
         new GUI();
